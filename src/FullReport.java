24390166e80e84ba4eee1163903ce030cb6c8bd9
ebykov <ebykov@griddynamics.com>
2022-01-13 15:42:14 +0300

    full report working well with new data class

--- src/FullReport.java
+++ src/FullReport.java
@@ -1,5 +1,6 @@
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -16,79 +17,88 @@ public class FullReport {
                     For which date you want to see results?
                     Please input if format:
                     d Month year - Day of week - Time in format hh:mm""");
-            String dateOfFinish = scanner.nextLine();
-            System.out.println("Full(Generating report date: " + dateOfFinish + "):");
+            String dayOfReport = scanner.nextLine();
+            System.out.println("Full(Generating report date: " + dayOfReport + "):");
             Scanner scan = new Scanner(reader);
 
-            while (scan.hasNextLine()) {
-                //This block will parse data from file into stringBuilder which would be returned System.lineSeparator()
-                String firstLines = scan.nextLine();
-                StringBuilder oneReport = new StringBuilder();
-                oneReport.append(System.lineSeparator()).append(firstLines.trim().split(": ")[1]);
-                firstLines = scan.nextLine();
-                oneReport.append("(").append(firstLines.trim().split(": ")[1])
-                        .append("). Work time: from 10:00 till 18:00");
-                firstLines = scan.nextLine();
-                String dateOfStart = firstLines.trim().split(": ")[1];
 
-                //Counting hours between our start and finish dates
-                int givenHours = HourCounter.HoursBetween(dateOfStart, dateOfFinish);
-                scan.nextLine();
-                scan.nextLine();
+            ArrayList<StudentData> studentsListParsed = new ArrayList<>();
+            /*This whole while block provides building list of studentData which is
+            class with student data parsed into data class*/
+            while (scan.hasNextLine()) {
                 String nextLine = null;
-                int neededHours;
-                String dateForEach = dateOfStart + " - 10:00";
-                //Getting time for each course in new cycle
+                ArrayList<String> parseToStudentData = new ArrayList<>();
+                int i = 0;
                 while (!Objects.equals(nextLine, "") && scan.hasNextLine()) {
                     nextLine = scan.nextLine();
-                    if (!Objects.equals(nextLine, "")) {
-                        String[] program = nextLine.trim().split("\\. +");
-                        try {
-                            //Hours needed to complete course which is now processing in cycle
-                            neededHours = Integer.parseInt(program[2]);
-                        } catch (IllegalArgumentException e) {
-                            throw new IllegalArgumentException(
-                                    String.format("Illegal argument in course %d duration in input file",
-                                            Integer.parseInt(program[1])));
-                        }
-                        //Appending general info about the course
-                        oneReport.append(System.lineSeparator()).append("  Course: ").append(program[1])
-                                .append(" - duration: ").append(program[2]).append(" hours")
-                                .append(System.lineSeparator());
-                        //Appending start date
-                        oneReport.append("      Date of start:").append(dateForEach).append(System.lineSeparator());
-                        //Appending finish date with hour added by my subfunction hourAdder
-                        try {
-                            dateForEach = HourAdder.hourAdder(dateForEach, neededHours);
-                            oneReport.append("      Date of finish:").
-                                    append(dateForEach).append(System.lineSeparator());
-                        } catch (IllegalArgumentException e) {
-                            throw new IllegalArgumentException("Error in processing date of start in input file");
-                        }
-
-                        //Adding how much time has passed / remains until completion for each course
-                        if (neededHours <= givenHours) {
-                            oneReport.append("  Training completed. ");
-                            int difference = givenHours - neededHours;
-                            int difDays = difference / 8;
-                            int difHours = difference % 8;
-                            if (difDays != 0) oneReport.append(difDays).append(" working days ").
-                                    append(difHours).append(" hours have passed since the end.");
-                            else oneReport.append(difHours).append(" hours have passed since the end.");
-                        } else {
-                            oneReport.append("  Training is not finished. ");
-                            int difference = neededHours - givenHours;
-                            int difDays = difference / 8;
-                            int difHours = difference % 8;
-                            if (difDays != 0) oneReport.append(difDays).append(" working days ").
-                                    append(difHours).append(" hours are left until the end.");
-                            else oneReport.append(difHours).append(" are left until the end.");
-                        }
-                        //Decrementing givenHours by value of hours of each course
-                        givenHours -= neededHours;
-                    }
+                    parseToStudentData.add(nextLine);
                 }
-                System.out.println(oneReport);
+                studentsListParsed.add(new StudentData(parseToStudentData));
+            }
+
+            //Writing to terminal report for each student
+            for (StudentData studentData : studentsListParsed) {
+                //Those lines would write name of student and his curriculum to stringBuilder
+                StringBuilder oneStudentReport = new StringBuilder();
+                oneStudentReport.append(System.lineSeparator()).append(studentData.name);
+                oneStudentReport.append("(").append(studentData.curriculum)
+                        .append("). Work time: from 10:00 till 18:00.");
+
+
+                /*There are counting to realize are courses finished or not
+                givenHours are hours between date of learning start and inputted from terminal date */
+                int givenHours = HourCounter.HoursBetween(studentData.dateOfLearningStart, dayOfReport);
+                int neededHours = 0;
+
+                String dateForEach = studentData.dateOfLearningStart + " - 10:00";
+                /*Ending our output string in StringBuilder by adding how many hours have
+                left before courses are finished/ have passed after finishing all courses*/
+                for (Course course : studentData.courses) {
+                    try {
+                        //Hours needed to complete course which is now processing in cycle
+                        neededHours = course.duration;
+                    } catch (IllegalArgumentException e) {
+                        throw new IllegalArgumentException(
+                                String.format("Illegal argument in course %d duration in input file",
+                                        Integer.parseInt(course.name)));
+                    }
+                    //Appending general info about the course
+                    oneStudentReport.append(System.lineSeparator()).append("  Course: ").append(course.name)
+                            .append(" - duration: ").append(course.duration).append(" hours")
+                            .append(System.lineSeparator());
+                    //Appending start date
+                    oneStudentReport.append("      Date of start:").append(dateForEach).append(System.lineSeparator());
+                    //Appending finish date with hour added by my subfunction hourAdder
+                    try {
+                        dateForEach = HourAdder.hourAdder(dateForEach, neededHours);
+                        oneStudentReport.append("      Date of finish:").
+                                append(dateForEach).append(System.lineSeparator());
+                    } catch (IllegalArgumentException e) {
+                        throw new IllegalArgumentException("Error in processing date of start in input file");
+                    }
+
+                    //Adding how much time has passed / remains until completion for each course
+                    if (neededHours <= givenHours) {
+                        oneStudentReport.append("  Training completed. ");
+                        int difference = givenHours - neededHours;
+                        int difDays = difference / 8;
+                        int difHours = difference % 8;
+                        if (difDays != 0) oneStudentReport.append(difDays).append(" working days ").
+                                append(difHours).append(" hours have passed since the end.");
+                        else oneStudentReport.append(difHours).append(" hours have passed since the end.");
+                    } else {
+                        oneStudentReport.append("  Training is not finished. ");
+                        int difference = neededHours - givenHours;
+                        int difDays = difference / 8;
+                        int difHours = difference % 8;
+                        if (difDays != 0) oneStudentReport.append(difDays).append(" working days ").
+                                append(difHours).append(" hours are left until the end.");
+                        else oneStudentReport.append(difHours).append(" are left until the end.");
+                    }
+                    //Decrementing givenHours by value of hours of each course
+                    givenHours -= neededHours;
+                }
+                System.out.println(oneStudentReport);
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
